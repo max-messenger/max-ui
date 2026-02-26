c126900fee7746dd3193a719fd8fe08cacd2ce87
Артур Мавлидов <mavlidov.a@juicedev.org>
2026-02-26 17:48:55 +0300

    fix(MaxUI): update config deps

--- src/components/MaxUI/MaxUI.tsx
+++ src/components/MaxUI/MaxUI.tsx
@@ -28,7 +28,7 @@ export const MaxUI = forwardRef<HTMLDivElement, MaxUIProps>((props, ref) => {
   const config = useMemo<MaxUIContextInterface>(() => ({
     colorScheme,
     platform
-  }), []);
+  }), [colorScheme]);
 
   const rootClassName = clsx(
     styles.MaxUI,
